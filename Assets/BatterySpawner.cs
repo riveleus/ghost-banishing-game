6ceed671a2e6e884248a5cf149631903f9042cae
LawlietRama <auliarama3499@gmail.com>
2021-03-18 19:58:40 +0700

    added BatterySpawner
    implement battery spawns in certain positions

---
+++ Assets/BatterySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatterySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject batteryPrefab;
+
+    [SerializeField]
+    private Transform[] spawnLocations;
+
+    /*[SerializeField]
+    private int totalBatteryMinimum = 3;*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < spawnLocations.Length/2; i++)
+        {
+            Instantiate(batteryPrefab, spawnLocations[Random.Range(0, spawnLocations.Length -1)].position, batteryPrefab.transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
