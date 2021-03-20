cfb98ef43c0bb91b680b276e5243ee23467cc96f
LawlietRama <auliarama3499@gmail.com>
2021-03-20 15:43:06 +0700

    add music, sound and bind system
    as description

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -22,7 +22,7 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //PlayMusic(levelMusicToPlay);
+        PlayMusic(levelMusicToPlay);
     }
 
     public void PlayMusic(int musicToPlay)
--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -15,6 +15,11 @@ public class Ghost : MonoBehaviour
     [SerializeField] Transform[] randomPositionBetweenPlayer;
     private bool isAttacking;
 
+    public float healthBar = 10;
+    public float heartbeatCounter = 10;
+    public bool isBinded = false;
+    public Transform flashlight;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,38 +27,81 @@ public class Ghost : MonoBehaviour
 
     void Update()
     {
-        if (escaping)
+        if(healthBar>0 && !isBinded)
         {
-            movement = (transform.position - player.transform.position).normalized;
-            moveSpeed = escapeSpeed;
+            if (escaping)
+            {
+                if (!AudioManager.instance.sfx[1].isPlaying)
+                {
+                    AudioManager.instance.PlaySFX(1);
+                }
+                healthBar -= Time.deltaTime;
+                movement = (transform.position - player.transform.position).normalized;
+                moveSpeed = escapeSpeed;
+            }
+            else
+            {
+                movement = (player.transform.position - transform.position).normalized;
+                moveSpeed = normalSpeed;
+            }
+
+            if (movement.x < 0 && isFacingRight || movement.x > 0 && !isFacingRight)
+            {
+                Flip();
+            }
+
+            if (isAttacking)
+            {
+                if(!AudioManager.instance.sfx[0].isPlaying)
+                {
+                    AudioManager.instance.PlaySFX(0);
+                }
+                
+                player.GetDamage();
+            }
+            else if (!isAttacking && AudioManager.instance.sfx[0].isPlaying)
+            {
+                AudioManager.instance.sfx[0].Stop();
+            }
         }
         else
         {
-            movement = (player.transform.position - transform.position).normalized;
-            moveSpeed = normalSpeed;
+            if(healthBar >= 10)
+            {
+                isBinded = false;
+                this.transform.SetParent(null);
+                transform.localEulerAngles = new Vector3(0, 0, 0);
+                Disappear();
+            }
+            else
+            {
+                isBinded = true;
+
+                this.transform.SetParent(flashlight);
+                isFacingRight = !isFacingRight;
+                transform.localPosition = new Vector3(6, 0, 0);
+                transform.localEulerAngles = new Vector3(0, 0, -flashlight.rotation.eulerAngles.z);
+                healthBar += Time.deltaTime;
+            }
         }
 
-        if (movement.x < 0 && isFacingRight || movement.x > 0 && !isFacingRight)
-        {
-            Flip();
-        }
-
-        if (isAttacking)
-        {
-            player.GetDamage();
-        }
+        
     }
 
     void FixedUpdate()
     {
-        if (Vector2.Distance(transform.position, player.transform.position) > 5.3f)
+        if (!isBinded)
         {
-            rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, player.transform.position) > 5.3f)
+            {
+                rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+            }
+            else if (escaping)
+            {
+                rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+            }
         }
-        else if (escaping)
-        {
-            rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
-        }
+        
     }
 
     public void Escape(bool state)
@@ -69,8 +117,12 @@ public class Ghost : MonoBehaviour
 
     public void Disappear()
     {
-        int index = Random.Range(0, randomPositionBetweenPlayer.Length);
-        transform.position = randomPositionBetweenPlayer[index].position;
+        if (!isBinded)
+        {
+            int index = Random.Range(0, randomPositionBetweenPlayer.Length);
+            transform.position = randomPositionBetweenPlayer[index].position;
+        }
+        
     }
 
     void OnTriggerEnter2D(Collider2D other)
